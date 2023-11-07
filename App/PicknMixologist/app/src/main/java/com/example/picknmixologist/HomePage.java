08d099325771462c23b3af4271aae4450b40ebb0
Alexander Willcox <alexander.willcox@students.plymouth.ac.uk>
2023-11-07 20:32:59 +0000

    HomePage Layout
    The homepage layout has been done and now staring the "Whats New" page layout

---
+++ App/PicknMixologist/app/src/main/java/com/example/picknmixologist/HomePage.java
@@ -0,0 +1,11 @@
+package com.example.picknmixologist;
+
+public class HomePage {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
--- App/PicknMixologist/app/src/main/java/com/example/picknmixologist/MainActivity.kt
+++ App/PicknMixologist/app/src/main/java/com/example/picknmixologist/MainActivity.kt
@@ -2,10 +2,10 @@ package com.example.picknmixologist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
+    
+
 }
