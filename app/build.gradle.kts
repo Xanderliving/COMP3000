4b0a1cc072d21a353257e80d108134dcf5ae33b4
Alexander Willcox <alexander.willcox@students.plymouth.ac.uk>
2024-02-16 13:13:20 +0000

    Fix code from android studio update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-
-
 plugins {
     id("com.android.application")
 }
@@ -11,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.example.picknmixologist"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 26
         versionCode = 1
         versionName = "1.0"
 
@@ -36,12 +34,9 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.android.volley:volley:1.2.1")
-    implementation("com.github.bumptech.glide:glide:4.12.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.5.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Pick'n'Mixologist"
+rootProject.name = "Pick 'n' Mixologist"
 include(":app")
+ 
